ce4f0e4425498c553672881fc5804de6f4b12741
Alena Nemtsova <jelenatbs@gmail.com>
2023-12-14 20:48:13 +0300

    code style improvements

--- script.js
+++ script.js
@@ -1,42 +1,16 @@
 'use strict';
 
-let title = "JavaScript-course";
-let screens = "Простые, Сложные, Интерактивные";
-let screenPrice = 1000;
+let title = prompt("Как называется ваш проект?");
+let screens = prompt("Какие типы экранов нужно разработать?");
+let screenPrice = +prompt("Сколько будет стоить данная работа?");
+let adaptive = confirm("Нужен ли адаптив на сайте?");
+let service1 = prompt("Какой дополнительный тип услуги нужен?");
+let servicePrice1 = +prompt("Сколько это будет стоить?");
+let service2 = prompt("Какой дополнительный тип услуги нужен?");
+let servicePrice2 = +prompt("Сколько это будет стоить?");
 let rollback = 2;
-let fullPrice = 2000;
-let adaptive = true;
-
-console.log(typeof title);
-console.log(typeof fullPrice);
-console.log(typeof adaptive);
-console.log(screens.length);
-console.log(`Стоимость верстки экранов ${screenPrice} рублей/долларов/гривен/юани`);
-console.log(`Стоимость разработки сайта ${fullPrice} рублей/долларов/гривен/юани`);
-console.log(screens.toLowerCase().split(", "));
-console.log(fullPrice * (rollback / 100));
-
-const titleAnswer = prompt("Как называется ваш проект?");
-title = titleAnswer;
-
-const screensAnswer = prompt("Какие типы экранов нужно разработать?", "пример: Простые, Сложные, Интерактивные");
-screens = screensAnswer;
-
-const screenPriceAnswer = +prompt("Сколько будет стоить данная работа?");
-screenPrice = screenPriceAnswer;
-
-const adaptiveAnswer = confirm("Нужен ли адаптив на сайте?");
-adaptive = adaptiveAnswer;
-
-const service1 = prompt("Какой дополнительный тип услуги нужен?");
-const servicePrice1 = +prompt("Сколько это будет стоить?");
-const service2 = prompt("Какой дополнительный тип услуги нужен?");
-const servicePrice2 = +prompt("Сколько это будет стоить?");
-
-fullPrice = screenPrice + servicePrice1 + servicePrice2;
-
-const servicePercentPrice = Math.ceil(fullPrice - fullPrice * (rollback / 100));
-console.log(servicePercentPrice);
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
+let servicePercentPrice = Math.ceil(fullPrice - fullPrice * (rollback / 100));
 
 switch (true) {
     case fullPrice >= 30000:
@@ -53,3 +27,13 @@ switch (true) {
         break
 };
 
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+
+console.log(screens.length);
+console.log(`Стоимость верстки экранов ${screenPrice} рублей/долларов/гривен/юани`);
+console.log(`Стоимость разработки сайта ${fullPrice} рублей/долларов/гривен/юани`);
+console.log(screens.toLowerCase().split(", "));
+console.log(fullPrice * (rollback / 100));
+console.log(servicePercentPrice);
